d2dc0fb02faf3e87e0cb36e5f80f6548f7c5e414
Luke Michals <lgm1234@protonmail.com>
2024-04-26 23:49:46 -0700

    feat: refactor to abstract out handleInput util

--- src/components/spreadsheet-3s1t/spreadsheet-3s1t.tsx
+++ src/components/spreadsheet-3s1t/spreadsheet-3s1t.tsx
@@ -3,7 +3,6 @@ import { Component, State, h } from '@stencil/core';
 interface Column {
   prop: string;
   name: string;
-  cellTemplate?: (item: any) => any;
 }
 
 interface Row {
@@ -27,9 +26,22 @@ export class Spreadsheet3s1t {
     { name: 'Item 3', details: 'Details of Item 3' },
   ];
 
+  handleInput(event: Event, rowIndex: number, propName: string) {
+    console.log('Update Cell', { event, rowIndex, propName });
+    const inputElement = event.target as HTMLInputElement;
+    const updatedValue = inputElement.value;
+
+    const updatedRows = this.rows.map((row, index) => {
+      if (index === rowIndex) {
+        return { ...row, [propName]: updatedValue };
+      }
+      return row;
+    });
+
+    this.rows = updatedRows;
+  }
+
   renderTable() {
-    console.log('Render');
-    console.log('Rows: ', this.rows);
     return (
       <table>
         <thead>
@@ -41,23 +53,10 @@ export class Spreadsheet3s1t {
         </thead>
         <tbody>
           {this.rows.map((row, rowIndex) => (
-            <tr>
+            <tr key={`row-${rowIndex}`}>
               {this.columns.map(column => (
-                <td>
-                  <input
-                    type="text"
-                    value={row[column.prop]}
-                    onInput={(event: any) => {
-                      const updatedRow: Row = { ...row, [column.prop]: event.data };
-                      const updatedRows = [
-                        ...this.rows.slice(0, rowIndex), // rows before the edited row
-                        updatedRow, // updated row
-                        ...this.rows.slice(rowIndex + 1), // rows after the edited row
-                      ];
-
-                      this.rows = updatedRows;
-                    }}
-                  />
+                <td key={`cell-${rowIndex}-${column.prop}`}>
+                  <input type="text" value={row[column.prop]} onInput={(event: Event) => this.handleInput(event, rowIndex, column.prop)} />
                 </td>
               ))}
             </tr>
@@ -68,7 +67,6 @@ export class Spreadsheet3s1t {
   }
 
   render() {
-    console.log('Spreadsheet Render');
     return (
       <div>
         <h3>Hello from 3s1t</h3>
