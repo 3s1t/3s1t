11471af6f72eccf9cf8c5f0aa5c9b91e25320678
Luke Michals <lgm1234@protonmail.com>
2024-04-26 22:29:31 -0700

    delete revogrid

--- src/components/spreadsheet-3s1t/spreadsheet-3s1t.tsx
+++ src/components/spreadsheet-3s1t/spreadsheet-3s1t.tsx
@@ -20,38 +20,12 @@ export class Spreadsheet3s1t {
   @State() columns: Column[] = [];
   @State() items: Item[] = [];
 
-  connectedCallback() {
-    console.log('Spreadsheet Connected');
-    const columns = [
-      { prop: 'name', name: 'First column' },
-      {
-        prop: 'details',
-        name: 'Second column',
-        cellTemplate: (createElement, props) => {
-          return createElement(
-            'div',
-            {
-              style: { backgroundColor: 'red' },
-              class: { 'inner-cell': true },
-            },
-            props.model[props.prop] || '',
-          );
-        },
-      },
-    ];
-    const items = [];
-    for (let i = 0; i < 10; i++) {
-      items.push({ name: 'New item', details: `Item ${i}` });
-    }
-    this.columns = columns;
-    this.items = items;
-  }
-
   render() {
-    console.log("Spreadsheet Render")
+    console.log('Spreadsheet Render');
     return (
       <div>
-        <revo-grid class="grid-component" columns={this.columns} source={this.items}></revo-grid>
+        <h3>Hello from 3s1t</h3>
+        <section>Table goes here</section>
       </div>
     );
   }
